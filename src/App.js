cbfd047813242ea991f3ef4cf922c16fea466492
chamodya wimansha <chamodyawimansha@gmail.com>
2020-08-29 11:57:28 +0530

    basic structure doen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="row">
+    //   <div className="container">
+    //     <div className="site-header col-md-12">
+    //       <h1>Markdown Previewer</h1>
+    //     </div>
+    //     <div className="editor-container col-md-6">ss</div>
+    //     <div className="previewer-container col-md-6">ss</div>
+    //   </div>
+    // </div>
+
+    <div className="container">
+      <div className="site-header">
+        <h1>Markdown Previewer</h1>
+      </div>
+      <div className="row">
+        <div className="col-sm-12 mb-2">
+          <div className="card editor-container">
+            <div className="card-body">
+              <h5 className="card-title">Editor</h5>
+              <textarea id="editor"></textarea>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-12">
+          <div className="card previewer-container">
+            <div className="card-body">
+              <h5 className="card-title">Previewer</h5>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
