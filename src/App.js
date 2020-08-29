5708bdedc3de79b327bbc284577b87742731bb57
chamodya wimansha <chamodyawimansha@gmail.com>
2020-08-29 14:20:07 +0530

    marked library added.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
+import marked from "marked";
 
-function App() {
-  return (
-    // <div className="row">
-    //   <div className="container">
-    //     <div className="site-header col-md-12">
-    //       <h1>Markdown Previewer</h1>
-    //     </div>
-    //     <div className="editor-container col-md-6">ss</div>
-    //     <div className="previewer-container col-md-6">ss</div>
-    //   </div>
-    // </div>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "", output: "" };
 
-    <div className="container">
-      <div className="site-header">
-        <h1>Markdown Previewer</h1>
-      </div>
-      <div className="row">
-        <div className="col-sm-12 mb-2">
-          <div className="card editor-container">
-            <div className="card-body">
-              <h5 className="card-title">Editor</h5>
-              <textarea id="editor"></textarea>
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value,
+      output: marked(event.target.value),
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="site-header">
+          <h1>Markdown Previewer</h1>
+        </div>
+        <div className="row">
+          <div className="col-sm-12 mb-2">
+            <div className="card editor-container">
+              <div className="card-body">
+                <h5 className="card-title">Editor</h5>
+                <textarea
+                  id="editor"
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-12">
+            <div className="card previewer-container">
+              <div className="card-body">
+                <h5 className="card-title">Previewer</h5>
+                <div
+                  id="preview"
+                  // dangerouslySetInnerHTML={{ __html: this.state.output }}
+                />
+              </div>
             </div>
           </div>
         </div>
-        <div className="col-sm-12">
-          <div className="card previewer-container">
-            <div className="card-body">
-              <h5 className="card-title">Previewer</h5>
-            </div>
-          </div>
-        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
